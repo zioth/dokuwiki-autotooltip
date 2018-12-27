c866485466ef4a3f76a9cd88b5e5172531f32a1e
Eli Fenton <junk1@zioth.com>
2018-12-27 11:27:36 -0600

    Added inclusion list to supplement exclusion list for the link-everything feature

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2,5 +2,5 @@
 $lang['style'] = 'A space-delimited set of default classes. "plugin-autotooltip__" will be prepended to ' .
 	'each one. Included options are "default," "plain" and "small"';
 $lang['delay'] = 'Time in miliseconds to wait before showing a tooltip';
-$lang['linkall_inclusions'] = 'When using the renderer plugin to add tooltips to all links, this is a regular expression for pages or namespaces to include. For example, /^wiki:|^stuff:/ inludes only links from the wiki and stuff namespaces. Leave blank to include all pages.';
+$lang['linkall_inclusions'] = 'When using the renderer plugin to add tooltips to all links, this is a regular expression for pages or namespaces to include. For example, "^wiki:|^stuff:" inludes only links from the wiki and stuff namespaces. Leave blank to include all pages.';
 $lang['linkall_exclusions'] = 'A regular expression for pages or namespaces to exclude. When combined with linkall_inclusions, this means "Include these pages, except those pages"';
